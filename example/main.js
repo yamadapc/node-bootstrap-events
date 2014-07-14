ea40c83e7fdf8288f45a496a8857574b6030bed4
yamadapc <tacla.yamada@gmail.com>
2014-07-13 23:37:37 -0300

    Add how EventEmitter2 support example.
    This is far for the ideal API for wildcard usage, but it works.

--- example/main.js
+++ example/main.js
@@ -7,6 +7,7 @@ var emitter = new events.EventEmitter();
 var path = path.join(__dirname, 'listeners');
 
 bootstrapEvents(emitter, path);
+assert(emitter.listeners('commands.*').length === 1);
 assert(emitter.listeners('commands.test_command.do_something').length === 1);
 assert(emitter.listeners('commands.test_command._doesnt_matter').length === 0);
 assert(emitter.listeners('commands.test_command.also_doesnt_matter').length === 0);
